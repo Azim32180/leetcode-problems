98e383437c5c1a57362df3ddc01e904742c68ce3
Azim <annayev.azim@gmail.com>
2025-05-29 12:21:55 -0700

    added problem description

--- problem-1/problem1.js
+++ problem-1/problem1.js
@@ -1,3 +1,14 @@
+/*
+Given an integer **`n`**, find the difference between the sums of its even and odd digits.
+
+Example
+
+- For **`n = 412`**, the output should be **`solution(n) = 5`**.
+    - The even digits are **`4`** and **`2`** and the odd digit is **`1`**. Therefore **`(4 + 2) - (1) = 5`**.
+- For **`n = 1203`**, the output should be **`solution(n) = -2`**.
+    - The even digits are **`2`** and **`0`** and the odd digits are **`1`**and **`3`**. Therefore **`(2 + 0) - (1 + 3) = -2`**.
+*/
+
 function solution(n) {
   let nStr = String(n);
   let evenSum = 0;
