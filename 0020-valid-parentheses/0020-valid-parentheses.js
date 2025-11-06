ce4f1584c57279b5371381583e4f97bc86918dcb
Azim Annayev <149440838+Azim32180@users.noreply.github.com>
2025-11-06 11:28:08 -0500

    Time: 0 ms (100%), Space: 55.6 MB (90.19%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    const stack = []
+
+    const obj = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    }
+
+    for (const char of s) {
+
+        if (char in obj) {
+           
+            if (stack.length === 0 || stack[stack.length - 1] !== obj[char]) {
+                return false 
+            }
+            stack.pop()
+        } else {
+            stack.push(char)
+        } 
+
+
+    }
+
+    if (stack.length === 0) {
+        return true
+    } else {
+        return false
+    }
+
+
+    
+};
