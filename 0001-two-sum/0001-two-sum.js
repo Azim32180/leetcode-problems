7cac9983a24227854192102a1acc5eb40bf4b5b9
Azim Annayev <149440838+Azim32180@users.noreply.github.com>
2025-11-05 23:35:50 -0500

    Time: 0 ms (100%), Space: 57.8 MB (26.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -8,7 +8,7 @@ var twoSum = function(nums, target) {
 let obj = {}
 
 for (let i = 0; i < nums.length; i++) {
-
+    
     const compliment = target - nums[i] 
 
     if (compliment in obj) return [obj[compliment], i]
